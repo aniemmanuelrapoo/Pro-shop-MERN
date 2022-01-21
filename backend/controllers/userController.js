c7413bcc16f62f3cfae535b215cb0057dc7a5156
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-01-21 04:26:46 +0100

    get user by Id endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -128,3 +128,17 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 })
 export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser }
+
+// @desc    Get user by ID
+// @routes  GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+
+  if(user){
+    res.json(user)
+  }else{
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
