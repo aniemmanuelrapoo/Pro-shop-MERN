e339adc45d1530046282ac4b1a1a352775837fdf
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-01-22 00:02:09 +0100

    get user by id and update user endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -127,7 +127,6 @@ const deleteUser = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser }
 
 // @desc    Get user by ID
 // @routes  GET /api/users/:id
@@ -142,3 +141,30 @@ const getUserById = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
+
+// @desc    Update user
+// @routes  PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  }else{
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser }
