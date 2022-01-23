7e8652d7094d516dbbf15c8d10cf69b5c14dcb3d
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-01-23 01:24:18 +0100

    update user functionality

--- myapp/src/actions/userActions.js
+++ myapp/src/actions/userActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { ORDER_LIST_MY_REST } from "../constants/orderConstants";
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_DETAILS_REST, USER_LIST_REQUEST, USER_LIST_SUCCESS, USER_LIST_FAIL, USER_LIST_REST, USER_DELETE_REQUEST, USER_DELETE_SUCCESS, USER_DELETE_FAIL} from "../constants/userConstants";
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_DETAILS_REST, USER_LIST_REQUEST, USER_LIST_SUCCESS, USER_LIST_FAIL, USER_LIST_REST, USER_DELETE_REQUEST, USER_DELETE_SUCCESS, USER_DELETE_FAIL, USER_UPDATE_REQUEST, USER_UPDATE_SUCCESS, USER_UPDATE_FAIL} from "../constants/userConstants";
 export const login = (email, password) => async (dispatch) => {
     try {
         dispatch({
@@ -175,6 +175,39 @@ export const deleteUser = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: USER_DELETE_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const updateUser = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.put(`/api/users/${user._id}`, user, config)
+
+        dispatch({
+            type: USER_UPDATE_SUCCESS
+        })
+
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: USER_UPDATE_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
--- myapp/src/screens/UserEditScreen.js
+++ myapp/src/screens/UserEditScreen.js
@@ -4,9 +4,10 @@ import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUser } from '../actions/userActions'
 import FormContainer from '../components/FormContainer'
 import { Link } from 'react-router-dom'
+import { USER_UPDATE_REST } from '../constants/userConstants'
 
 
 const UserEditScreen = () => {
@@ -21,20 +22,30 @@ const UserEditScreen = () => {
     const userDetails = useSelector(state => state.userDetails)
     const { loading, error, user } = userDetails
 
+    const userUpdate = useSelector(state => state.userUpdate)
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate
+
     const navigate = useNavigate();
 
     useEffect(() => {
-       if(!user.name || user._id !== id){
-        dispatch(getUserDetails(id))
+       if(successUpdate){
+        dispatch({ type: USER_UPDATE_REST })
+        navigate('/admin/userlist')
        }else{
-           setName(user.name)
-           setEmail(user.email)
-           setIsAdmin(user.isAdmin)
+        if(!user.name || user._id !== id){
+            dispatch(getUserDetails(id))
+        }else{
+            setName(user.name)
+            setEmail(user.email)
+            setIsAdmin(user.isAdmin)
+        }
        }
-    }, [user, dispatch, id])
+       
+    }, [user, dispatch, id, successUpdate, navigate])
 
     const submitHandler = (e) => {
         e.preventDefault()
+        dispatch(updateUser({_id: id, name, email, isAdmin}))
     }
     
     return (
@@ -45,6 +56,8 @@ const UserEditScreen = () => {
         
             <FormContainer>
                 <h1>Edit User</h1>
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
                 {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                     <Form onSubmit={submitHandler}>
                         <Form.Group controlId='name' className="mb-3">
