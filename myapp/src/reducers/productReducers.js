b4dd7e32edb273c140ff08a536904aa132b2a52a
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-01-23 04:22:49 +0100

    admin create product

--- myapp/src/reducers/productReducers.js
+++ myapp/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-import { PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
+import { PRODUCT_CREATE_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_REST, PRODUCT_CREATE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
 
 
 
@@ -36,6 +36,21 @@ export const productDeleteReducer = (state = {}, action) => {
             return { loading: false, success: true }
         case PRODUCT_DELETE_FAIL:
             return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+export const productCreateReducer = (state = {}, action) => {
+    switch (action.type) {
+        case PRODUCT_CREATE_REQUEST:
+            return { loading: true }
+        case PRODUCT_CREATE_SUCCESS:
+            return { loading: false, success: true, product: action.payload }
+        case PRODUCT_CREATE_FAIL:
+            return { loading: false, error: action.payload }
+        case PRODUCT_CREATE_REST:
+            return {}
         default:
             return state
     }
--- myapp/src/screens/ProductListScreen.js
+++ myapp/src/screens/ProductListScreen.js
@@ -2,15 +2,15 @@ import React, { useEffect } from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Button, Table, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts, deleteProduct } from '../actions/productActions'
+import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
+import { PRODUCT_CREATE_REST } from '../constants/productConstants'
 
 const ProductListScreen = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    let { id } = useParams();
 
     const productList = useSelector(state => state.productList)
     const { products, error, loading } = productList
@@ -21,23 +21,30 @@ const ProductListScreen = () => {
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
+    const productCreate = useSelector(state => state.productCreate)
+    const { loading: loadingCreate, success: successCreate, error: errorCreate, product: createdProduct } = productCreate
+
     useEffect(() => {
-        if(userInfo && userInfo.isAdmin){
-            dispatch(listProducts())
-        }else{
+        dispatch({ type: PRODUCT_CREATE_REST })
+        if(!userInfo.isAdmin){
             navigate('/login')
         }
+
+        if(successCreate){
+            navigate(`/admin/products/${createdProduct._id}/edit`)
+        }else{
+            dispatch(listProducts())
+        }
         
-    }, [dispatch, navigate, userInfo, successDelete])
+    }, [dispatch, navigate, userInfo, successDelete, successCreate, createdProduct])
 
     const deleteHandler = (id) => {
         if(window.confirm('Are you sure')){
             dispatch(deleteProduct(id))
         }
     }
-    const createProductHandler = (product) => {
-        //create product
-        console.log('create product')
+    const createProductHandler = () => {
+        dispatch(createProduct())
     }
 
     return (
@@ -54,6 +61,10 @@ const ProductListScreen = () => {
             </Row>
             {loadingDelete && <Loader />}
             {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+
+            {loadingCreate && <Loader />}
+            {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
+
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                 <Table striped bordered hover responsive className='table-sm'>
                     <thead>
