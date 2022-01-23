26f7e06f6acd84718a7617cdc8b4bd2155708e11
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-01-23 02:01:05 +0100

    admin product list

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditSreen from "./screens/UserEditScreen"
+import ProductListScreen from "./screens/ProductListScreen"
 
 const App = () => {
   return (
@@ -35,6 +36,7 @@ const App = () => {
               <Route path="" element={<CartScreen />} />
             </Route>
             <Route path="/admin/userlist" element={<UserListScreen />} />
+            <Route path="/admin/productlist" element={<ProductListScreen />} />
             <Route path="admin/user/:id/edit" element={<UserEditSreen />} />
             <Route path="/" element={<HomeScreen />} exact />
           </Routes>
